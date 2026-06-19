ccb47eb6ad6dd82bd4c53175db373563760760c3
Elmir-23 <115034736+Elmir-23@users.noreply.github.com>
2026-06-19 12:29:14 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -71,10 +71,10 @@ function AppInner({ isOwner, isDeveloper, userEmail, onSignOut }) {
     openDeliveryEntry, adjDelivery, saveDeliveryEntry,
     saveDebtCollection, openEditEntry, saveEditEntry,
     saveEditDebt, saveEditCollected, saveExpense, deleteExpense,
-    resetAllData, calcStats, calcExpenses, restoreBackup,
+    resetAllData, calcStats, calcExpenses,
     saveShops, addShop, removeShop, confirmRemoveShop,
     savePrices, changePin, saveHandover, confirmHandover, saveKassaAdjustment,
-    saveSweet,
+    restoreBackup,
   } = useAppData(userEmail);
 
   const TODAY = todayStr();
@@ -158,7 +158,6 @@ function AppInner({ isOwner, isDeveloper, userEmail, onSignOut }) {
               expVals={expVals} setExpVals={setExpVals}
               saveExpense={saveExpense} deleteExpense={deleteExpense}
               saveHandover={saveHandover}
-              saveSweet={saveSweet}
             />
           )}
           {view === "session" && <SessionScreen db_data={db_data} TODAY={TODAY} selShop={selShop} setView={setView} setCollectedInput={setCollectedInput} openDeliveryEntry={openDeliveryEntry} />}
@@ -194,7 +193,7 @@ function AppInner({ isOwner, isDeveloper, userEmail, onSignOut }) {
               </div>
               {ownerTab === "dashboard" && <Dashboard db_data={db_data} dashPeriod={dashPeriod} setDashPeriod={setDashPeriod} calcStats={calcStats} calcExpenses={calcExpenses} editDebtShop={editDebtShop} setEditDebtShop={setEditDebtShop} editDebtVal={editDebtVal} setEditDebtVal={setEditDebtVal} saveEditDebt={saveEditDebt} saveHandover={saveHandover} confirmHandover={confirmHandover} saveKassaAdjustment={saveKassaAdjustment} saveExpense={saveExpense} deleteExpense={deleteExpense} />}
               {ownerTab === "reports" && <Reports db_data={db_data} repPeriod={repPeriod} setRepPeriod={setRepPeriod} calcStats={calcStats} shopKura={shopKura} shopRail={shopRail} toast$={toast$} />}
-              {ownerTab === "edit" && <EditSection db_data={db_data} editDate={editDate} setEditDate={setEditDate} editView={editView} setEditView={setEditView} editSelShop={editSelShop} setEditSelShop={setEditSelShop} editSelSess={editSelSess} editEntryVals={editEntryVals} adjEdit={adjEdit} saveEditEntry={saveEditEntry} openEditEntry={openEditEntry} editCollected={editCollected} setEditCollected={setEditCollected} saveEditCollected={saveEditCollected} editDebtShop={editDebtShop} setEditDebtShop={setEditDebtShop} editDebtVal={editDebtVal} setEditDebtVal={setEditDebtVal} saveEditDebt={saveEditDebt} saveHandover={saveHandover} saveKassaAdjustment={saveKassaAdjustment} saveSweet={saveSweet} />}
+              {ownerTab === "edit" && <EditSection db_data={db_data} editDate={editDate} setEditDate={setEditDate} editView={editView} setEditView={setEditView} editSelShop={editSelShop} setEditSelShop={setEditSelShop} editSelSess={editSelSess} editEntryVals={editEntryVals} adjEdit={adjEdit} saveEditEntry={saveEditEntry} openEditEntry={openEditEntry} editCollected={editCollected} setEditCollected={setEditCollected} saveEditCollected={saveEditCollected} editDebtShop={editDebtShop} setEditDebtShop={setEditDebtShop} editDebtVal={editDebtVal} setEditDebtVal={setEditDebtVal} saveEditDebt={saveEditDebt} saveHandover={saveHandover} saveKassaAdjustment={saveKassaAdjustment} />}
               {ownerTab === "parametrler" && <Parametrler db_data={db_data} settPrices={settPrices} setSettPrices={setSettPrices} savePrices={savePrices} pinOld={pinOld} setPinOld={setPinOld} pinNew={pinNew} setPinNew={setPinNew} changePin={changePin} shopEdits={shopEdits} setShopEdits={setShopEdits} newShopName={newShopName} setNewShopName={setNewShopName} addShop={addShop} removeShop={removeShop} saveShops={saveShops} />}
               {ownerTab === "developer" && isDeveloper && <Developer db_data={db_data} archives={archives} resetConfirm={resetConfirm} setResetConfirm={setResetConfirm} resetPinBuf={resetPinBuf} setResetPinBuf={setResetPinBuf} resetPinErr={resetPinErr} setResetPinErr={setResetPinErr} resetAllData={resetAllData} restoreBackup={restoreBackup} toast$={toast$} />}
               <div style={{ padding: "0 1rem 1.5rem" }}>
